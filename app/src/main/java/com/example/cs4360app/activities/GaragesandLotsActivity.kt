a1563d663afb4caeafd422c2c51aee88753e39c0
“[Seth <sjone193@msudenver.edu>
2024-11-12 14:54:32 -0700

    Price Changes

--- app/src/main/java/com/example/cs4360app/activities/GaragesandLotsActivity.kt
+++ app/src/main/java/com/example/cs4360app/activities/GaragesandLotsActivity.kt
@@ -21,25 +21,25 @@ class GaragesandLotsActivity : AppCompatActivity() {
         val garageList = listOf(
             LocationData("Tivoli Garage", R.drawable.tivoli_garage, "$8.00", "Sheltered with convenient for access to Auraria Parkway"),
             LocationData("7th Street Garage", R.drawable.seventh_street_garage, "$8.00", "Sheltered at the center of the main campus"),
-            LocationData("5th Street Garage", R.drawable.fifth_street_garage, "$8.00", "Sheltered with a high amount of spaces available")
+            LocationData("5th Street Garage", R.drawable.fifth_street_garage, "$6.50", "Sheltered with a high amount of spaces available")
         )
 
         // Data for parking lots in alphabetical order
         val lotList = listOf(
             LocationData("Aspen Lot", R.drawable.aspen_lot, "$5.00", "A well prices lot near the Campus Village building"),
-            LocationData("Beech Lot", R.drawable.beech_lot, "$6.00", "A quiet lot, less crowded"),
-            LocationData("Birch Lot", R.drawable.birch_lot, "$5.50", "Near MSU sports facilities"),
-            LocationData("Cherry Lot", R.drawable.cherry_lot, "$8.00", "Less crowded, with easy access to I-25"),
+            LocationData("Beech Lot", R.drawable.beech_lot, "$5.00", "A quiet lot, less crowded"),
+            LocationData("Birch Lot", R.drawable.birch_lot, "$5.00", "Near MSU sports facilities"),
+            LocationData("Cherry Lot", R.drawable.cherry_lot, "$6.50", "Less crowded, with easy access to I-25"),
             LocationData("Dogwood Lot", R.drawable.dogwood_lot, "$8.00", "Popular among students for the AES building"),
-            LocationData("Elm Lot", R.drawable.elm_lot, "$6.25", "A large lot with many parking spaces"),
-            LocationData("Fir Lot", R.drawable.fir_lot, "$5.50", "Near the Administration building"),
-            LocationData("Holly Lot", R.drawable.holly_lot, "$6.00", "Convenient lot with many spaces at the center of campus"),
-            LocationData("Juniper Lot", R.drawable.juniper_lot, "$5.25", "Less traffic, quieter"),
-            LocationData("Maple Lot", R.drawable.maple_lot, "$6.00", "A smaller lot near the Cherry Creek academic building"),
-            LocationData("Nutmeg Lot", R.drawable.nutmeg_lot, "$6.50", "A very limited lot mostly used by faculty"),
-            LocationData("Oak Lot", R.drawable.oak_lot, "$5.75", "A very limited lot mostly used by faculty"),
+            LocationData("Elm Lot", R.drawable.elm_lot, "$6.50", "A large lot with many parking spaces"),
+            LocationData("Fir Lot", R.drawable.fir_lot, "$6.50", "Near the Administration building"),
+            LocationData("Holly Lot", R.drawable.holly_lot, "$8.00", "Convenient lot with many spaces at the center of campus"),
+            LocationData("Juniper Lot", R.drawable.juniper_lot, "$6.50", "Less traffic, quieter"),
+            LocationData("Maple Lot", R.drawable.maple_lot, "$8.00", "A smaller lot near the Cherry Creek academic building"),
+            LocationData("Nutmeg Lot", R.drawable.nutmeg_lot, "$8.00", "A very limited lot mostly used by faculty"),
+            LocationData("Oak Lot", R.drawable.oak_lot, "$8.00", "A very limited lot mostly used by faculty"),
             LocationData("Spruce Lot", R.drawable.spruce_lot, "$8.00", "Great lot at the center of the MSU campus"),
-            LocationData("Walnut Lot", R.drawable.walnut_lot, "$5.50", "A great lot with great savings")
+            LocationData("Walnut Lot", R.drawable.walnut_lot, "$5.00", "A great lot with great savings")
         )
 
 
